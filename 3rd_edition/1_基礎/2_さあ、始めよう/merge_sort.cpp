0ecd2c0757d708c544b0877a267216378d6f2b97
kurrrru <kurrrru8850@gmail.com>
2024-08-05 02:27:26 +0900

    [Update] merge_sort

--- 3rd_edition/1_基礎/2_さあ、始めよう/merge_sort.cpp
+++ 3rd_edition/1_基礎/2_さあ、始めよう/merge_sort.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void	merge(vector<int> &A, int p, int q, int r)
+void merge(std::vector<int> &A, int p, int q, int r)
 {
 	int n1 = q - p + 1;
 	int n2 = r - q;
 	int L[n1 + 1];
 	int R[n2 + 1];
-
 	for (int i = 0; i < n1; i++)
 		L[i] = A[p + i];
 	for (int j = 0; j < n2; j++)
@@ -19,27 +18,20 @@ void	merge(vector<int> &A, int p, int q, int r)
 	for (int k = p; k <= r; k++)
 	{
 		if (L[i] <= R[j])
-		{
-			A[k] = L[i];
-			i++;
-		}
+			A[k] = L[i++];
 		else
-		{
-			A[k] = R[j];
-			j++;
-		}
+			A[k] = R[j++];
 	}
 }
 
-void	merge_sort(vector<int> &A, int p, int r)
+void merge_sort(std::vector<int> &A, int p, int r)
 {
-	if (p < r)
-	{
-		int q = (p + r) / 2;
-		merge_sort(A, p, q);
-		merge_sort(A, q + 1, r);
-		merge(A, p, q, r);
-	}
+	if (p >= r)
+		return;
+	int q = (p + r) / 2;
+	merge_sort(A, p, q);
+	merge_sort(A, q + 1, r);
+	merge(A, p, q, r);
 }
 
 int main()
